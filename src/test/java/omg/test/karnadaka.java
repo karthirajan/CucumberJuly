cc60df70a3ee1c03a651b78cefe423c06b2f6a1f
thamizhsvk30 <thamizhsvk30@gmail.com>
2020-07-23 08:08:17 +0530

    added two more packages

---
+++ src/test/java/omg/test/karnadaka.java
@@ -0,0 +1,24 @@
+package omg.test;
+
+public class karnadaka extends tamilNadu {
+
+@Override
+	public void dieselRate() {
+	System.out.println(50);
+	}
+@Override
+	public void petrolRate() {
+	System.out.println(80);
+}
+public void crudOil() {
+	System.out.println(45);
+}
+public static void main(String[] args) {
+	karnadaka k = new karnadaka();
+	k.dieselRate();
+	k.petrolRate();
+	k.crudOil();
+
+	
+}	
+}
